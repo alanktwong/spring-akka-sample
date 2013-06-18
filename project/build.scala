a9689a9908c49b4fe4f6b178a9e17db194f0888d
awong <alan_kt_wong@yahoo.com>
2013-06-18 16:06:31 -0400

    downgrade buildScalaVersion

--- project/build.scala
+++ project/build.scala
@@ -10,14 +10,15 @@ object BuildSettings {
   val buildProject      = "spring-akka-travel"
   val buildOrganization = "org.springframework.samples"
   val buildVersion      = "1.0.0-BUILD-SNAPSHOT"
-  val buildScalaVersion = "2.10.1"
+  val buildScalaVersion = "2.9.3"
+  val buildJavaVersion   = "1.6"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization  := buildOrganization,
     version       := buildVersion,
     scalaVersion  := buildScalaVersion,
     scalacOptions ++= Seq("-unchecked", "-deprecation"),
-    javacOptions  ++= Seq("-target", "1.6", "-source", "1.6"),
+    javacOptions  ++= Seq("-target", buildJavaVersion, "-source", buildJavaVersion),
     shellPrompt  := ShellPrompt.buildShellPrompt,
     licenses := Seq("LGPL v3" -> url("http://www.gnu.org/licenses/lgpl.txt"))
   )
@@ -65,38 +66,32 @@ object Resolvers {
 }
 
 object Dependencies {
-  val provided = "provided"
-  val test = "test"
-  val runtime = "runtime"
-
-  val javaVer   = "1.6"
-    
-  val springOrg = "org.springframework"
-  val springVer = "3.0.5.RELEASE"
-  val springSecurityVer = "3.0.8.RELEASE"
-  val springWebflowVer = "2.2.1.RELEASE"
-  val aspectjVer = "1.6.10"
-  val slf4jVer   = "1.6.1"
-
-  val akkaOrg = "com.typesafe.akka"
-  val akkaVer = "2.0.3"
-
+  import BuildSettings._
   
-  val hsqldb = "org.hsqldb" % "hsqldb" % "1.8.0.10" % runtime
-  val junit = "junit" % "junit" % "4.10" % test
-  val easymock = "org.easymock" % "easymock" % "2.5.2" % test
+  val javaVer   = buildJavaVersion
+  val provided  = "provided"
+  val test      = "test"
+  val runtime   = "runtime"
+  val compile   = "compile"
+  val container = "container"
+  val containerCompile = container + ", " + compile
 
-  val scalatest = "org.scalatest" %% "scalatest" % "1.9.1" % test
-  val scalazFull = "org.scalaz" %% "scalaz-full" % "6.0.4"
-  val guava = "com.google.guava" % "guava" % "12.0"
+  val hsqldb   = "org.hsqldb"   % "hsqldb"   % "1.8.0.10" % runtime
+  val junit    = "junit"        % "junit"    % "4.10"     % test
+  val easymock = "org.easymock" % "easymock" % "2.5.2"    % test
+
+  val scalatest  = "org.scalatest"    %% "scalatest"   % "1.9.1" % test
+  val scalazFull = "org.scalaz"       %% "scalaz-full" % "6.0.4"
+  val guava      = "com.google.guava" %  "guava"       % "12.0"
 
   val json4s =  "org.json4s" %% "json4s-native" % "3.2.4"
 
-  val jodaTime = "joda-time" % "joda-time" % "1.6.2"
+  val jodaTime     = "joda-time" % "joda-time"         % "1.6.2"
   val jodaTimeTags = "joda-time" % "joda-time-jsptags" % "1.1.1" % runtime
-  val xerces   = "xerces" % "xercesImpl" % "2.9.1" % runtime
+  val xerces       = "xerces"    % "xercesImpl"        % "2.9.1" % runtime
   
-  val aspectj  = "org.aspectj" % "aspectjrt" % aspectjVer
+  val aspectjVer  = "1.6.10"
+  val aspectj     = "org.aspectj" % "aspectjrt" % aspectjVer
 
   val hibernateDependencies = Seq(
       "org.hibernate" % "hibernate-entitymanager" % "3.6.0.Final",
@@ -106,37 +101,45 @@ object Dependencies {
   val tiles = "org.apache.tiles" % "tiles-jsp" % "2.2.2" exclude("commons-logging", "commons-logging-api")
   val cglib = "cglib" % "cglib-nodep" % "2.2"
   
+  val akkaOrg = "com.typesafe.akka"
+  val akkaVer = "2.0.3"
   val akkaDependencies = Seq(
       akkaOrg % "akka-actor" % akkaVer
       )
 
+  val springOrg         = "org.springframework"
+  val springVer         = "3.0.5.RELEASE"
+  val springSecurityVer = "3.0.8.RELEASE"
+  val springWebflowVer  = "2.2.1.RELEASE"
   val springCoreDependencies = Seq(
-      springOrg % "org.springframework.context" % springVer exclude("commons-logging", "commons-logging-api"),
-      springOrg % "org.springframework.jdbc" % springVer,
+      springOrg % "org.springframework.context"     % springVer exclude("commons-logging", "commons-logging-api"),
+      springOrg % "org.springframework.jdbc"        % springVer,
       springOrg % "org.springframework.transaction" % springVer,
-      springOrg % "org.springframework.orm" % springVer,
-      springOrg % "org.springframework.web" % springVer,
+      springOrg % "org.springframework.orm"         % springVer,
+      springOrg % "org.springframework.web"         % springVer,
       springOrg % "org.springframework.web.servlet" % springVer,
-      springOrg % "org.springframework.test" % springVer % test
+      springOrg % "org.springframework.test"        % springVer % test
       )
-  
   val springSecurityDependencies = Seq(
-      "org.springframework.security" % "org.springframework.security.core" % springSecurityVer,
-      "org.springframework.security" % "org.springframework.security.web" % springSecurityVer exclude("commons-logging", "commons-logging-api"),
-      "org.springframework.security" % "org.springframework.security.config" % springSecurityVer exclude("commons-logging", "commons-logging-api"),
+      "org.springframework.security" % "org.springframework.security.core"    % springSecurityVer,
+      "org.springframework.security" % "org.springframework.security.web"     % springSecurityVer exclude("commons-logging", "commons-logging-api"),
+      "org.springframework.security" % "org.springframework.security.config"  % springSecurityVer exclude("commons-logging", "commons-logging-api"),
       "org.springframework.security" % "org.springframework.security.taglibs" % springSecurityVer
       )
   val springWebflowDependencies = Seq(
       "org.springframework.webflow"  % "org.springframework.webflow" % springWebflowVer,
-      "org.springframework.webflow"  % "org.springframework.js" % springWebflowVer
+      "org.springframework.webflow"  % "org.springframework.js"      % springWebflowVer
       )
   val springScala      = "org.springframework.scala" % "spring-scala" % "1.0.0.M1"
-  val springRoo        = "org.springframework.roo" % "org.springframework.roo.annotations" % "1.1.0.RELEASE"
-  val springDependencies = springCoreDependencies ++ springSecurityDependencies ++ springWebflowDependencies ++ Seq(springRoo, springScala)
+  val springRoo        = "org.springframework.roo"   % "org.springframework.roo.annotations" % "1.1.0.RELEASE"
+  val springDependencies = springCoreDependencies ++
+                           springSecurityDependencies ++
+                           springWebflowDependencies ++
+                           Seq(springRoo, springScala)
   
 
   val commonsLogging = "commons-logging" % "commons-logging" % "1.1.1" % runtime
-
+  val slf4jVer          = "1.6.1"
   val loggingDependencies = Seq(
       "log4j" % "log4j" % "1.2.16" excludeAll(
         ExclusionRule(organization = "com.sun.jdmk"),
@@ -144,25 +147,26 @@ object Dependencies {
         ExclusionRule(organization = "javax.mail"),
         ExclusionRule(organization = "javax.jms")
       ),
-      "org.slf4j" % "slf4j-api" % slf4jVer % runtime,
+      "org.slf4j" % "slf4j-api"      % slf4jVer % runtime,
       "org.slf4j" % "jcl-over-slf4j" % slf4jVer % runtime,
-      "org.slf4j" % "slf4j-log4j12" % slf4jVer % runtime
+      "org.slf4j" % "slf4j-log4j12"  % slf4jVer % runtime
       )
   
   val testDependencies   = Seq(scalatest, junit, easymock)
   
   val j2eeDependencies = Seq(
-      "javax.inject" % "javax.inject" % "1",
-      "javax.servlet" % "servlet-api" % "2.5" % provided,
-      "javax.servlet" % "jstl" % "1.2" % provided,
-      "javax.validation" % "validation-api"  % "1.0.0.GA",
-      "javax.servlet.jsp" % "jsp-api" % "2.1" % provided
+      "javax.inject"      % "javax.inject"    % "1",
+      "javax.servlet"     % "servlet-api"     % "2.5"       % provided,
+      "javax.servlet"     % "jstl"            % "1.2"       % provided,
+      "javax.validation"  % "validation-api"  % "1.0.0.GA",
+      "javax.servlet.jsp" % "jsp-api"         % "2.1"       % provided
       )
+  val jettyVer            = "8.0.0.M3"
   val jettyDependencies = Seq(
-      "org.mortbay.jetty" % "servlet-api" % "3.0.20100224" % "provided",
-      "org.eclipse.jetty" % "jetty-server" % "8.0.0.M3" % "container, compile",
-      "org.eclipse.jetty" % "jetty-util" % "8.0.0.M3" % "container, compile",
-      "org.eclipse.jetty" % "jetty-webapp" % "8.0.0.M3" % "container, compile"
+      "org.mortbay.jetty" % "servlet-api"  % "3.0.20100224" % provided,
+      "org.eclipse.jetty" % "jetty-server" % jettyVer       % containerCompile,
+      "org.eclipse.jetty" % "jetty-util"   % jettyVer       % containerCompile,
+      "org.eclipse.jetty" % "jetty-webapp" % jettyVer       % containerCompile
       )
   val utilityDependencies = Seq(xerces, aspectj, jodaTime, jodaTimeTags, hsqldb, tiles, cglib)
   
